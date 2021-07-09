33110e1381450fe3d4ab6f1c0738d00698aa697f
Thanh <kimthanh.cao@gmail.com>
2021-07-09 13:36:42 +0200

    Reworked function writeMealPlan

--- assets/js/recipe-randomizer.js
+++ assets/js/recipe-randomizer.js
@@ -10,8 +10,8 @@ let intolerances = [];
 const totalNutrientsDisplay = $("#total-nutrients-absolute");
 const totalNutrientsChart = $("#total-nutrients-chart");
 
-// const myapiKey = "1f698cd1ba074580acc428ca007720bc";
-const apiKey = "09c17d8e03d043c49e345a14a780221e";
+const apiKey = "1f698cd1ba074580acc428ca007720bc";
+// const apiKey2 = "09c17d8e03d043c49e345a14a780221e";
 
 let mealListId = [];
 
@@ -20,23 +20,23 @@ dietPreferenceList.on("change", getUserDiet);
 intoleranceList.on("change", getUserIntolerances);
 userMealPreference.on("submit", e => {
   e.preventDefault();
-  randomizeMealPlan();
+  handleUserPreferences();
 });
 
-function randomizeMealPlan() {
-  $('#meal-plan').removeClass('d-none');
+function handleUserPreferences() {
+  $("#meal-plan").removeClass("d-none");
   mealPlanDisplay.empty();
   totalNutrientsDisplay.empty();
   totalNutrientsChart.empty();
   fetchMealPlan();
 }
 // reset button to clear all data
-$('button[type="reset"]').on('click', function() {
-  diet = '';
+$('button[type="reset"]').on("click", function () {
+  diet = "";
   intolerances.splice(0);
   mealListId.splice(0);
-  mealPlanDisplay.empty();
-})
+  $("#meal-plan").addClass("d-none");
+});
 
 // functions to gather user's search query based on diet preference and intolerances and parse queries to API call
 function getUserDiet(e) {
@@ -60,77 +60,93 @@ function getUserIntolerances(e) {
   return intolerances;
 }
 
+// The results from ajax do not have data on the meals' image urls. After api call is finished, the function will extract IDs of all the meals into an array and then the meal ID array is parsed into another api call in order to get all the neccessary data to then finally write to document
 function fetchMealPlan() {
   $.when(
     $.ajax({
-    url: `https://api.spoonacular.com/mealplanner/generate?apiKey=${apiKey}&timeFrame=day&diet=${diet.toLowerCase()}&exclude=${intolerances.toString()}&type=main%20course,side%20dish,dessert,appetizer,salad,bread,breakfast,soup,fingerfood,snack`,
-    method: "GET"
-  })).then(function(results) {
-    console.log(results);
-    console.log(results.meals);
-    console.log(results.nutrients);
-    let mealList = results.meals;
-    extractMealListId(mealList);
-    writeTotalNutrientsBreakdown(results.nutrients);
-  }).done(function () {
-    console.log(mealListId);
-    writeMealPlan(mealListId);
-  })
+      url: `https://api.spoonacular.com/mealplanner/generate?apiKey=${apiKey}&timeFrame=day&diet=${diet.toLowerCase()}&exclude=${intolerances.toString()}&type=breakfast,lunch,dinner`,
+      method: "GET"
+    })
+  )
+    .then(function (results) {
+      console.log(results);
+      console.log(results.meals);
+      console.log(results.nutrients);
+      let mealList = results.meals;
+      extractMealListId(mealList);
+      writeTotalNutrientsBreakdown(results.nutrients);
+    })
+    .done(function () {
+      console.log(mealListId);
+      writeMealPlan(mealListId);
+    });
 }
 
-function extractMealListId (mealList) {
+function extractMealListId(mealList) {
+  //empty previous mealListId array and then assign a new one for new meal plan
   mealListId = [];
-  // for (let meal of mealList) {
-  //   console.log(meal.id);
-  //   mealListId.push(meal.id);
-  //   console.log(mealListId);
-  // }
   $.each(mealList, function (index, value) {
     console.log(value);
     console.log(value.id);
     mealListId.push(value.id);
     console.log(mealListId);
-  })
+  });
   return mealListId;
 }
 
-// functions to display search results on to page
+// below functions to display search results on to page in order breakfast, lunch, dinner
 function writeMealPlan(mealListId) {
-  let mealPlanHtml = "";
-  $.each(mealListId, function(index, value) {
-    $.ajax({
-      url: `https://api.spoonacular.com/recipes/${value}/information?apiKey=${apiKey}&includeNutrition=true`,
-      method: 'GET'
-    }).done(function(recipeData) {
-      console.log(recipeData);
-      console.log(recipeData.image);
-      mealPlanHtml = `
+  //use Promise.all() to get all the json data ready before looping the data to write meal cards in correct order (breakfast, lunch, dinner) based on data's index
+  let breakfastResponse = fetch(
+    `https://api.spoonacular.com/recipes/${mealListId[0]}/information?apiKey=${apiKey}&includeNutrition=true`
+  ),
+    lunchResponse = fetch(
+      `https://api.spoonacular.com/recipes/${mealListId[1]}/information?apiKey=${apiKey}&includeNutrition=true`
+    ),
+    dinnerResponse = fetch(
+      `https://api.spoonacular.com/recipes/${mealListId[2]}/information?apiKey=${apiKey}&includeNutrition=true`
+    );
+
+  Promise.all([breakfastResponse, lunchResponse, dinnerResponse])
+    .then(responses => {
+      return Promise.all(
+        responses.map(response => {
+          return response.json();
+        })
+      );
+    })
+    .then(dataArray => {
+      console.log(dataArray);
+      dataArray.forEach(data => {
+        console.log(data.id);
+        let mealCardHtml = "";
+        mealCardHtml = `
       <div class="card mb-3">
           <div class="row row-cols-2 pt-3 pb-2">
-              <h3 class="col my-auto ps-4">${writeMealCardTitle(index)}</h3>
+              <h3 class="col my-auto ps-4 meal-card-title">${writeMealCardTitle(dataArray.indexOf(data))}</h3>
               <div class="col text-end pe-4">
-                  <button class="btn btn-secondary" onclick="findNewRecipe();"><i class="fas fa-random"></i></button>
+                  <button class="btn btn-secondary find-new-meal-btn"><i class="fas fa-random"></i></button>
               </div>
           </div>
-          <div class="row g-0">
+          <div class="row g-0 meal-card-data">
               <div class="col-md-4 my-auto">
-                  <img src="${(recipeData.image)}" class="w-100 h-auto px-3" alt="${recipeData.title}">
+                  <img src="${data.image}" class="w-100 h-auto px-3" alt="${data.title}">
               </div>
               <div class="col-md-8">
                   <div class="card-body">
-                      <h4 class="card-title">${recipeData.title}</h4>
+                      <h4 class="card-title">${data.title}</h4>
                       <p class="card-text">
-                          <small class="text-muted">Ready in: ${recipeData.readyInMinutes} minutes</small>
-                          <small class="text-muted float-end">Servings: ${recipeData.servings}</small>
+                          <small class="text-muted">Ready in: ${data.readyInMinutes} minutes</small>
+                          <small class="text-muted float-end">Servings: ${data.servings}</small>
                       </p>
                       <div class="row row-cols-2">
                           <div class="col text-center">
-                              <span class="border py-2 px-3 d-block mb-2">Calories: ${recipeData.nutrition.nutrients[0].amount}</span>
-                              <span class="border py-2 px-3 d-block">Fat: ${recipeData.nutrition.nutrients[1].amount}g</span>
+                              <span class="border py-2 px-3 d-block mb-2">Calories: ${data.nutrition.nutrients[0].amount}</span>
+                              <span class="border py-2 px-3 d-block">Fat: ${data.nutrition.nutrients[1].amount}g</span>
                           </div>
                           <div class="col text-center">
-                              <span class="border py-2 px-3 d-block mb-2">Protein: ${recipeData.nutrition.nutrients[8].amount}</span>
-                              <span class="border py-2 px-3 d-block">Carbs: ${recipeData.nutrition.nutrients[3].amount}</span>
+                              <span class="border py-2 px-3 d-block mb-2">Protein: ${data.nutrition.nutrients[8].amount}</span>
+                              <span class="border py-2 px-3 d-block">Carbs: ${data.nutrition.nutrients[3].amount}</span>
                           </div>
                       </div>
                   </div>
@@ -138,18 +154,21 @@ function writeMealPlan(mealListId) {
           </div>
       </div>
       `;
-      mealPlanDisplay.append(mealPlanHtml);
+        mealPlanDisplay.append(mealCardHtml);
+      });
+    })
+    .catch(err => {
+      console.log(err);
     });
-  });
-};
+}
 
-function writeMealCardTitle (index) {
+function writeMealCardTitle(index) {
   if (index === 0) {
-    return 'Breakfast';
+    return "Breakfast";
   } else if (index === 1) {
-    return 'Lunch';
+    return "Lunch";
   } else if (index === 2) {
-    return 'Dinner';
+    return "Dinner";
   }
 }
 
@@ -175,7 +194,3 @@ function writeTotalNutrientsBreakdown(nutrients) {
     `;
   totalNutrientsDisplay.append(caloBreakdownHtml);
 }
-
-// function findNewRecipe(
-
-// )
