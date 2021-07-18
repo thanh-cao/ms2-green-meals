107bff2d0e921df14fd1eb3e224e6ae9ead2aa55
Thanh <kimthanh.cao@gmail.com>
2021-07-18 13:40:13 +0200

    Add functions to save/load localStorage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 // light/dark theme toggle
 const themeToggle = $('.theme-switch input[type="checkbox"]');
-const themePreference = localStorage.getItem("theme");
+const themePreference = loadFromLocalStorage("theme");
+
+themeToggle.on('change', toggleDarkMode);
 
 if (themePreference === "dark") {
   $("body").addClass("dark-mode");
@@ -10,11 +12,20 @@ if (themePreference === "dark") {
 function toggleDarkMode(e) {
   if (e.target.checked) {
     $("body").addClass("dark-mode");
-    localStorage.setItem("theme", "dark");
+    saveToLocalStorage("theme", "dark");
   } else {
     $("body").removeClass("dark-mode");
-    localStorage.setItem("theme", "light");
+    saveToLocalStorage("theme", "light");
   }
 }
 
-themeToggle.on('change', toggleDarkMode);
+//functions to save and retrieve an item to local storage
+function saveToLocalStorage(dataName, dataToSave) {
+  //convert object into json string to save to local storage
+  localStorage.setItem(dataName, JSON.stringify(dataToSave));
+}
+
+function loadFromLocalStorage(dataName) {
+  //retrieve json string and parse it back to javascript object
+  return JSON.parse(localStorage.getItem(dataName));
+}
