c6bb15d75c1830d0d59db3d79a19ae71578b0226
Thanh <kimthanh.cao@gmail.com>
2021-08-01 04:39:15 +0200

    Add logic if there is no data for ingredients or instructions

--- assets/js/recipe-details.js
+++ assets/js/recipe-details.js
@@ -16,7 +16,7 @@ $(window).ready(function () {
     $('#servings').text(recipeData.servings);
     $('.recipe-img').attr('src', recipeData.image);
     $('.recipe-img').attr('alt', recipeData.title);
-    $('.recipe-instructions').html(recipeInstructions);
+    $('#recipe-instructions').html(recipeInstructions);
     writeNutrientsAbsolute('calories', recipeData.nutrition.nutrients, 'mealData');
     writeNutrientsAbsolute('protein', recipeData.nutrition.nutrients, 'mealData');
     writeNutrientsAbsolute('fat', recipeData.nutrition.nutrients, 'mealData');
@@ -24,21 +24,21 @@ $(window).ready(function () {
     drawCaloricBreakdownChart(recipeData.nutrition.caloricBreakdown, 'mealData');
 })
 
-$('.back-to-meal-plan').on('click', function() {
+$('.back-to-meal-plan').on('click', function () {
     history.back();
     saveToLocalStorage('loadMealPlan', 'true');
 })
 
 function generateOrderedRecipeInstructions(recipeData) {
-    let stepsArray = recipeData.analyzedInstructions[0].steps;
-    if (stepsArray) {
+    let instructionArray = recipeData.analyzedInstructions;
+    if (instructionArray.length !== 0) {
         let instructionSteps = '<ol>';
-        stepsArray.forEach(step => {
+        instructionArray[0].steps.forEach(step => {
             instructionSteps += `<li>${step.step}</li>`;
         })
         instructionSteps += '</ol>';
         return instructionSteps;
-    } else if (recipeData.sourceUrl) {
+    } else if (instructionArray.length === 0 && recipeData.sourceUrl) {
         return `<p>Detailed instructions can be found at <a href="${recipeData.sourceUrl}" target="_blank">${recipeData.sourceName}</a></p>`
     } else {
         return `<p>No detailed instructions found</p>`;
@@ -48,16 +48,21 @@ function generateOrderedRecipeInstructions(recipeData) {
 function generateIngredientList(recipeData) {
     let ingredientImgBaseUrl = 'https://spoonacular.com/cdn/ingredients_100x100/';
     let ingredientRows = '';
-    recipeData.extendedIngredients.forEach(ingredient => {
-        ingredientRows += `
-        <li class="row mb-3">
-            <span class="ingredient-img col-3"><img src="${ingredientImgBaseUrl}${ingredient.image}" width="50" height="50"></span>
-            <span class="ingredient-name col-6 my-auto">${capitalizeFirstLetter(ingredient.name)}</span>
-            <span class="ingredient-qty col-3 my-auto">${ingredient.amount} ${ingredient.unit}</span>
-        </li>
-        `;
-    })
-    return ingredientRows;
+
+    if (recipeData.extendedIngredients) {
+        recipeData.extendedIngredients.forEach(ingredient => {
+            ingredientRows += `
+            <li class="row mb-3">
+                <span class="ingredient-img col-3"><img src="${ingredientImgBaseUrl}${ingredient.image}" width="50" height="50"></span>
+                <span class="ingredient-name col-6 my-auto">${capitalizeFirstLetter(ingredient.name)}</span>
+                <span class="ingredient-qty col-3 my-auto">${ingredient.amount} ${ingredient.unit}</span>
+            </li>
+            `;
+        })
+        return ingredientRows;
+    } else {
+        return `<p>Ingredient list can be found at <a href="${recipeData.sourceUrl}" target="_blank">${recipeData.sourceName}</a></p>`
+    }
 }
 
 // function to add to list in localStorage
