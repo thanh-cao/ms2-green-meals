f6e770d6822916f42ab2e43ffb9d22f47acde18a
Thanh <kimthanh.cao@gmail.com>
2021-08-01 06:31:58 +0200

    adjust logic to reload user's prev chosen diet

--- assets/js/load-meal-plan.js
+++ assets/js/load-meal-plan.js
@@ -3,14 +3,20 @@ $(window).ready(function () {
     if (loadFromLocalStorage('loadMealPlan') === 'true') {
         let mealPlanNutrients = loadFromLocalStorage('totalNutrientBreakdown');
         $("#meal-plan").removeClass("d-none");
-        diet = loadFromLocalStorage('userDiet');
         intolerances = loadFromLocalStorage('userIntolerances');
 
-        dietPreferenceList.each(function() {
-            if (this.id === loadFromLocalStorage('userDiet')) {
-                this.checked = true;
-            }
-        })
+        if (!loadFromLocalStorage('userDiet')) {
+            diet = 'vegetarian';
+            dietPreferenceList[0].checked = true;
+        } else {
+            diet = loadFromLocalStorage('userDiet');
+            dietPreferenceList.each(function() {
+                if (this.id === loadFromLocalStorage('userDiet')) {
+                    this.checked = true;
+                }
+            })    
+        }
+
         intoleranceList.each(function() {
             if (intolerances.includes(capitalizeFirstLetter(this.id))) {
                 this.checked = true;
