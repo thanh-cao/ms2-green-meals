78336ba94d40bebfa9fba2d1feb026f4a5a00656
Thanh <kimthanh.cao@gmail.com>
2021-08-02 12:46:23 +0200

    Add grocery.js to display grocery list

---
+++ assets/js/grocery.js
@@ -0,0 +1,28 @@
+$(window).ready(function () {
+    let retrievedGroceryList = loadFromLocalStorage('groceryList');
+    console.log(retrievedGroceryList);
+
+    if (retrievedGroceryList) {
+        let itemRows = '';
+        retrievedGroceryList.forEach(item => {
+            itemRows += `
+                <div class="row g-0 align-items-start">
+                    <div class="col-2 text-center">
+                        <input type="checkbox">
+                    </div>
+                    <div class="col-5">
+                        <p class="grocery-item">${capitalizeFirstLetter(item[0])}</p>
+                    </div>
+                    <div class="col-3">
+                        <p class="grocery-item-qty">${item[1]} ${item[2]}</p>
+                    </div>
+                    <div class="col-2 ps-0">
+                        <i class="fas fa-edit"></i>
+                        <i class="fas fa-trash-alt"></i>
+                    </div>
+                </div>
+                `;
+        });
+        $('#grocery-list-data').append(itemRows);
+    }
+})
