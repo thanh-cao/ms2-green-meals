bec6fa26575d6a1def4eace2e30bec4d82a7c34e
Thanh <kimthanh.cao@gmail.com>
2021-08-09 11:28:31 +0200

    add fuctions to edit quantity and remove item

--- assets/js/grocery.js
+++ assets/js/grocery.js
@@ -10,14 +10,13 @@ $(window).ready(function () {
                     <div class="col-2 text-center">
                         <input class="item-checkbox" type="checkbox">
                     </div>
-                    <div class="col-5">
+                    <div class="col-6">
                         <p class="grocery-item">${capitalizeFirstLetter(item[0])}</p>
                     </div>
-                    <div class="col-3">
-                        <p class="grocery-item-qty">${item[1]} ${item[2]}</p>
+                    <div class="col-3 pe-2 d-flex justify-content-between">
+                        <p class="grocery-item-qty d-inline w-75" contenteditable>${item[1]} ${item[2]}</p>
                     </div>
-                    <div class="col-2 ps-0">
-                        <i class="fas fa-edit"></i>
+                    <div class="col-1">
                         <i class="fas fa-trash-alt"></i>
                     </div>
                 </div>
@@ -27,10 +26,28 @@ $(window).ready(function () {
     }
 
     let checkboxes = $('.item-checkbox');
-    checkboxes.each(function() {
+    let qtyEditable = $('p.grocery-item-qty');
+    let removeBtns = $('i.fa-trash-alt');
+
+    checkboxes.each(function () {
+        $(this).on('click', function () {
+            $(this).parent().next().toggleClass('strikethrough').next().toggleClass('strikethrough');
+        })
+    })
+
+    qtyEditable.each(function () {
+        $(this).mouseenter(function () {
+            $(this).parent().append('<i class="fas fa-edit d-inline"></i>');
+        })
+        $(this).mouseleave(function () {
+            $(this).siblings('i').remove();
+        })
+    })
+
+    removeBtns.each(function() {
         $(this).on('click', function() {
-            console.log($(this).parent().siblings());
-            $(this).parent().siblings().toggleClass('strikethrough');
+            console.log($(this).parents()[1]);
+            $(this).parents()[1].remove();
         })
     })
 })
